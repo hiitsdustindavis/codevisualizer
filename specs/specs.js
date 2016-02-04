e311a20b7f2e66d2b2f0baa30d3a86bccc4d6ff6
AndrewTelleria <atelleria08@gmail.com>
2016-02-04 08:27:55 -0800

    nothing to commit

--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 describe('Question', function() {
   it("creates a new contact with the given properties", function() {
     var testContact = new Contact("Rita","Moreno");
@@ -8,8 +7,3 @@ describe('Question', function() {
     expect(testContact.addresses).to.eql([]);
   });
 });
-=======
-
->>>>>>> ee7bd86652cbd7f66c43c657d7690d6c209a8397
-=======
->>>>>>> 1b83665826d36c914286888723abaa931228e657
